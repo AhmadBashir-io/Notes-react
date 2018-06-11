52a287e485f590381bbf868bbef95f4d0743cad7
tjomega03 <37372913+tjomega03@users.noreply.github.com>
2018-06-11 15:23:39 +0000

    Installing Semantic Ui

--- notes-react/src/components/pages/HomePage.js
+++ notes-react/src/components/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => (
-    <div> 
+    <div className="ui container"> 
         <h1>HomePage</h1>
         <Link to="/login" >Login</Link>
     </div>
